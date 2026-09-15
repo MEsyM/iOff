9c9444ed13e80a450565e148bc222ac424fffe5a
MEsyM <80831783+MEsyM@users.noreply.github.com>
2026-09-15 11:30:15 +0200

    test: add Roborazzi screenshot plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,6 @@
-plugins { id("com.android.application") version "8.10.1" apply false; id("org.jetbrains.kotlin.android") version "2.2.10" apply false; id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false }
+plugins {
+    id("com.android.application") version "8.10.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
+    id("io.github.takahirom.roborazzi") version "1.73.0" apply false
+}
