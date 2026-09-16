d80652f37404f695964ede33577675f9452e4500
MEsyM <80831783+MEsyM@users.noreply.github.com>
2026-09-16 20:41:51 +0200

    Prepare iOff v0.6.1 build 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "cz.ioff.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.5.1"
+        versionCode = 7
+        versionName = "0.6.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
